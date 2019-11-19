dd51797312403b34cb4c1cc6cb1346d863c9ae66
nurpny <runnurrr@gmail.com>
2019-11-18 21:00:25 -0500

    Persistent Issue with Marker

--- src/marker.js
+++ src/marker.js
@@ -1,23 +1,30 @@
 // export into index.js
 const mapboxgl = require("mapbox-gl");
 
+const iconURLs = {
+  hotels: "http://i.imgur.com/D9574Cu.png",
+  restaurants: "http://i.imgur.com/cqR6pUI.png",
+  activities: "http://i.imgur.com/WbMOfMl.png"
+};
 
 
-const buildMarker = (type = 'hotel', coordinates) => {
+const buildMarker = (type, coordinates) => {
   const markerDomEl = document.createElement("div");
   markerDomEl.style.width = "32px";
   markerDomEl.style.height = "39px";
   // markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
-
-  if (type === 'activity') {
-    markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
-  }
-  if (type === 'hotel') {
-    markerDomEl.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)";
-  } if (type === 'restaurant') {
-    markerDomEl.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)";
-  }
-  return new mapboxgl.Marker(document.getElementById(markerDomEl)).setLngLat(coordinates)
+  switch (type) {
+    case 'activity':
+      markerDomEl.style.backgroundImage = iconURLs.activities;
+      break;
+    case 'hotel':
+      markerDomEl.style.backgroundImage = iconURLs.hotels;
+      break;
+    case 'restaurant':
+      markerDomEl.style.backgroundImage = iconURLs.restaurants;
+      break;
+    }
+  return new mapboxgl.Marker(markerDomEl).setLngLat(coordinates)
 }
 
 module.exports = buildMarker
